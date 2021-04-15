0b61d2deced95330a306892c2b6dd145c776e6fd
Oliver Zott <zott_oliver@web.de>
2021-04-15 18:29:42 +0200

    Prepare entities and database context

---
+++ HotelListingExample/Data/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListingExample.Data
+{
+    // Bridge between Entities and actual database
+    public class DatabaseContext : DbContext
+    {
+    }
+}
