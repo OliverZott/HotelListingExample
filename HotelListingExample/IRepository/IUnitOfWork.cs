7037704ffeca778da5c3b908be771b7b3d4c4b47
Oliver Zott <zott_oliver@web.de>
2021-05-02 19:40:04 +0200

    Implement unit of work pattern for managing generic repositories

---
+++ HotelListingExample/IRepository/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using HotelListingExample.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace HotelListingExample.IRepository
+{
+
+    // Service class that registers all versions of generic repository implementation 
+    // for generic class T.
+    interface IUnitOfWork : IDisposable
+    {
+
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+
+        // In the generic repository all changes are just STAGED!
+        Task Save();
+    }
+}
---
+++ HotelListingExample/Repository/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using HotelListingExample.Data;
+using HotelListingExample.IRepository;
+using System;
+using System.Threading.Tasks;
+
+namespace HotelListingExample.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        private readonly DatabaseContext _context;
+        private IGenericRepository<Country> _countries;
+        private IGenericRepository<Hotel> _hotels;
+
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            this._context = context;
+        }
+
+
+        // Compare property versions as "block body" vs "expression body"
+        public IGenericRepository<Country> Countries => _countries ??= new GenericRepository<Country>(_context);
+        public IGenericRepository<Hotel> Hotels
+        {
+            get
+            {
+                return _hotels ??= new GenericRepository<Hotel>(_context);
+            }
+        }
+
+
+        public void Dispose()
+        {
+            _context.Dispose();     // if Dispose is called, dispose of context, kill any memory of connection/resources, ... and use Garbage COllector
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
