077f8a658078dfd8e058b667870b4f77d0c00737
Oliver Zott <zott_oliver@web.de>
2021-05-20 15:35:41 +0200

    Create migrations for user identity.

--- HotelListingExample/Migrations/20210520131957_AddedIdentity.Designer.cs
+++ HotelListingExample/Migrations/20210520131957_AddedIdentity.Designer.cs
@@ -4,14 +4,16 @@ using HotelListingExample.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HotelListingExample.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210520131957_AddedIdentity")]
+    partial class AddedIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
